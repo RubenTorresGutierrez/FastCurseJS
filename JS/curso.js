563a3d49bc6ece71bc62e666e39813532e673dcb
Rubén Torres <rtorresgutierrez.guadalupe@alumnado.fundacionloyola.net>
2021-09-21 12:33:00 +0200

    V2

--- JS/curso.js
+++ JS/curso.js
@@ -1,86 +1,63 @@
-//curso.js
-//Esto es un comentario
+'use strict'
+console.log('JS Cargado');
 
-/* Esto es un comentario
-  con muchas líneas
-*/
+//Objetos
+let objeto = {
+  atributo1: 78,
+  nombre: 'Perico Palotes',
+  parejas: ['María', 'Pedro', 'Macario'],
+  metodo2: function(num){
+    console.log('Método 2: ' + num * 2);
+  }
+}
+console.log(objeto);
+console.log('Me llamo ' + objeto.nombre);
+console.log('Una de mis parejas es ' + objeto.parejas[1]);
 
-'use strict' //Activar modo estricto
-
-//Formas de salida
-console.log('Hola Mundo'); //Salida en consola
-document.write('Adiós mundo cruel'); //Salida en documento
-alert('Viva mi profe de programación'); //Salida en popup
-
-//Formas de entrada
-var num = prompt('Dime algo bonito:');
-console.log(num);
-
-//Declaración de variables
-var num; //Declara la variable
-num = 7; //Tipo int
-num = 'Zapato'; //Ahora se cambia a tipo String
-
-num = 3.141592; //Tipo
-num = 6.022E23; // Tipo 6'022 * 10^23
-var G = 6.67E-11;
-var texto = 'Zapato';
-var booleano = true; //Tipo booleano
-var nulo = null; //Tipo null
-var nose = undefined; //Tipo undefined
-
-var fecha = 12;
-
-var texto = 'Tacón alto'; //Permite redeclarar y reasignar
-var texto = 7; //Redeclaración porque lleva var
-texto = true; //Reasignación porque no lleva var y solo se le cambia el valor
-              // Alcance (scope) global/local (función)
-
-console.log(texto); // => true
 function funcion1(){
-  console.log(texto); // => undefined
-  var texto = 78;
-  console.log(texto); // => 78
+  document.write('Me encanta JavaScript<br/>');
 }
-funcion1()
-console.log(texto); // => true
+funcion1();
+let num = 7; //Declaro num y le asigno 7. Es tipo Number
+num = funcion1; //Le asigno funcion1. Es tipo Function
+num(); //La ejecuto.
 
-texto = 'En un lugar de la Mancha';
-const PI = 3.141592
+objeto.metodo = funcion1;
+objeto.metodo();
+objeto.metodo2(88);
 
-let var1 = 7; //let permite Reasignar pero no Redeclarar
-              //Alcance de bloque
-var1 = 8 //reasignar
-
-function funcion2(){
-  let var1 = 'Zapato'
+//Parámetros de funciones
+function suma(a, b = 4){
+  return (a + b);
 }
+console.log(suma(5, 8));
+console.log(suma(2));
 
-//Decisiones
-let haySol = true;
-if(haySol) console.log('¡Qué calor!');
-else console.log('Lleva paraguas');
 
-let num1 = 16
-switch(num1){
-  case 4:
-    console.log('sffaagagsfd');
-    break;
-  case 5:
-  case 'Hola':
-  case true:
-    console.log('asagdfsgfdgf');
-    break;
-  default:
+function ejecuta(callback){
+  console.log('Ejecutando...');
+  callback();
 }
 
-//Bucles
+ejecuta(funcion1);
 
-for(let i=0;i<7;i++)
- console.info(i);
-
-let i = 0;
-while(i < 10){
-  console.error('PFFFFFF');
-  i++;
+//Excepciones
+try{
+  let nombre = 42;
+  console.log(nombre.substr(2,4));
 }
+catch(excepcion){
+  console.log('Catch: ' + excepcion);
+}
+finally{
+  console.log('Ejecuto el finally');
+}
+
+//Arrays
+let vector = [1,2,3];
+let guarreria = [1,2,'asdfasdf', function(){console.log('PFFF');}, true];
+let matriz = [
+  [1,2,3],
+  [4,5,6],
+  [7,8,9]
+];
